3abca9b50cc30180902e30815aacde113e2efa63
Monique Aira Desengano <moniqueaira66@gmail.com>
2023-04-07 00:13:47 +0800

    Initial Design Added

---
+++ kanayon_arms/src/components/Admin/Admin.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Admin() {
+    return (
+        <>
+            <nav className="d-none d-md-block bg-white sidebar p-2">
+                <div className='container-fluid'>
+                    <div className='m-2'>
+                        <span className='brand-name fs-4'>Kanayon Inasal</span>
+                    </div>
+                    <hr className='text-dark' />
+                    <div className='list-group list-group-flush'>
+                        <a className='list-group-item py-2'>
+                            <i class="fa-solid fa-users fs-5 me-3"></i>
+                            <span >List of Users</span>
+                        </a>
+                        <a className='list-group-item py-2 '>
+                            <i className='fa-solid fa-basket-shopping fs-5 me-3'></i>
+                            <span >List of Orders</span>
+                        </a>
+                        <a className='list-group-item py-2'>
+                            <i className='fa-solid fa-boxes-stacked fs-5 me-3'></i>
+                            <span >Inventory Analysis</span>
+                        </a>
+                        <a className='list-group-item py-2'>
+                            <i className='fa-solid fa-right-from-bracket fs-5 me-3'></i>
+                            <span >Logout</span>
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Admin;
