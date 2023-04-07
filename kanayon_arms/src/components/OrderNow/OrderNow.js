283b8ed7d6f1071c5691f4c6c566887b42a4feca
LucidoML <lucidomarklester@gmail.com>
2023-04-08 00:12:34 +0800

    Fixed Errors

--- kanayon_arms/src/components/OrderNow/OrderNow.js
+++ kanayon_arms/src/components/OrderNow/OrderNow.js
@@ -5,7 +5,7 @@ function OrderNow() {
         <>
             <div class="offcanvas offcanvas-end" tabindex="-1" id="myOffcanvas">
                 <div class="offcanvas-header">
-                    <h5 class="offcanvas-title">Offcanvas Menu</h5>
+                    <h5 class="offcanvas-title">Offcanvas</h5>
                     <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body">
