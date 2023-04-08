26dab2ec26250fe7194f46e6823c041a3f40cdf5
kobellanes <118694734+kobellanes@users.noreply.github.com>
2023-04-08 08:42:03 +0800

    Fixing Errors of Admin Page

--- kanayon_arms/src/components/Admin/Admin.js
+++ kanayon_arms/src/components/Admin/Admin.js
@@ -1,52 +1,15 @@
 import React from 'react';
+import AdminNavbar from '../AdminNavBar/AdminNavbar';
+
 
 function Admin() {
     return (
         <>
-            <div className="container-fluid">
-                <div className="row min-vh-100 m-0 flex-column flex-md-row">
-                    <aside className="col-12 col-md-3 col-xl-2 p-0 m-0 bg-light">
-                        <nav className="navbar navbar-expand-md navbar-light bd-dark flex-md-column flex-row align-items-center py-2 text-start sticky-top "
-                            id="sidebar">
-                            <div className="text-center p-3">
-                                <a href="#" className="navbar-brand mx-0 font-weight-bold fs-3 text-nowrap">Kanayon Inasal</a>
-                            </div>
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
+            <AdminNavbar>
 
-                            <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
-                                <ul className="navbar-nav flex-column justify-content-center">
-                                    <li className="nav-item">
-                                        <a href="/userlist" className="nav-link active py-3 mt-3">
-                                            <i className="fa-solid fa-users fs-5 me-3"></i>
-                                            <span>List of Users</span>
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a href="#" className="nav-link active py-3">
-                                            <i className="fa-solid fa-basket-shopping fs-5 me-3"></i>
-                                            <span>List of Orders</span>
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a href="#" className="nav-link active py-3">
-                                            <i className="fa-solid fa-boxes-stacked fs-5 me-3"></i>
-                                            <span>Inventory Analysis</span>
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a href="#" className="nav-link active py-3">
-                                            <i className="fa-solid fa-right-from-bracket fs-5 me-3"></i>
-                                            <span>Logout</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </nav>
-                    </aside>
-                </div>
-            </div>
+            </AdminNavbar>
+
+            <h1 className="display-6">KAHUWEOUQHEIGQWEIQGEIQGEIGEQGWEGQ</h1>
         </>
     );
 }
--- kanayon_arms/src/components/UserPage/UserPage.js
+++ kanayon_arms/src/components/UserPage/UserPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import UserFunction from '../UserFunction/UserFunction';
+import AdminNavbar from '../AdminNavBar/AdminNavbar';
 
 function UserPage() {
     return (
         <>
+            <AdminNavbar></AdminNavbar>
             <main>
                 <div className="w-50 mx-auto mt-5">
                     <div className="border border-dark rounded p-3">
