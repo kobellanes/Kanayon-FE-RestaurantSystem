351d506f1071aabc9db57229091fc5acb68c0f85
LucidoML <lucidomarklester@gmail.com>
2023-04-09 00:16:31 +0800

    Order Now V0.1

--- kanayon_arms/src/components/OrderNow/OrderNow.js
+++ kanayon_arms/src/components/OrderNow/OrderNow.js
@@ -1,54 +1,104 @@
 import React from 'react';
 import "./OrderNow.css"
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
 function OrderNow() {
     return (
         <>
-            <h1 class="fw-bold fs-2 fs-lg-6 lh-sm mb-3">Tuloy Ang Bange!</h1>
-            <div class="card-group">
-                <div class="card container">
-                    <img src={require('../../assets/paa.jpg')} class="img-thumbnail h-100 card card-span h-100 text-white card-img-top" alt="..."></img>
-                    <div class="card-body">
-                        <h5 class="card-title fw-bold">PAA INASAL</h5>
-                        <p class="card-text fs-5">₱ 89</p>
-                        <p class="card-text">
-                            <small class="text fs-6">solo</small></p>
-                        <a href="select" class="btn btn-light">Select</a>
+            <Header>
+
+            </Header>
+            <h1 className="fw-bold fs-2 fs-lg-6 lh-sm mb-3 mt-5 pt-5">Tuloy Ang Bange! Tara Tikme!</h1>
+            <div className="card-group">
+                <div className="card container">
+                    <img src={require('../../assets/paa.jpg')} className="img-thumbnail card card-span text-white card-img-top" alt="..."></img>
+                    <div className="card-body">
+                        <h5 className="card-title fw-bold">PAA INASAL</h5>
+                        <p className="card-text fs-5">₱ 89</p>
+                        <p className="card-text">
+                            <small className="text fs-6">solo</small></p>
+                        <button className="btn btn-light" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu">Select</button>
+                    </div>
+
+                    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasMenu" aria-labelledby="offcanvasMenuLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasMenuLabel">Menu</h5>
+                            <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <img src={require('../../assets/paa.jpg')} className="img-thumbnail card card-span text-white card-img-top" alt="..."></img>
+                            <p className="text-dark">This is the content of my off-canvas menu.</p>
+                        </div>
                     </div>
                 </div>
 
-                <div class="card container">
-                    <img src={require('../../assets/pecho.jpg')} class="img-thumbnail h-100 card card-span h-100 text-white card-img-top" alt="..."></img>
-                    <div class="card-body">
-                        <h5 class="card-title fw-bold">PECHO INASAL</h5>
-                        <p class="card-text fs-5">₱ 92</p>
-                        <p class="card-text">
-                            <small class="text fs-6">solo</small></p>
-                        <a href="select" class="btn btn-light">Select</a>
+
+
+                <div className="card container">
+                    <img src={require('../../assets/pecho.jpg')} className="img-thumbnail card card-span text-white card-img-top" alt="..."></img>
+                    <div className="card-body">
+                        <h5 className="card-title fw-bold">PECHO INASAL</h5>
+                        <p className="card-text fs-5">₱ 92</p>
+                        <p className="card-text">
+                            <small className="text fs-6">solo</small></p>
+                        <button className="btn btn-light" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu">Select</button>
+                    </div>
+
+                    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasMenu" aria-labelledby="offcanvasMenuLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasMenuLabel">Menu</h5>
+                            <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <p className="text-dark">This is the content of my off-canvas menu.</p>
+                        </div>
                     </div>
                 </div>
 
-                <div class="card container">
-                    <img src={require('../../assets/liempo.jpg')} class="img-thumbnail h-100 card card-span h-100 text-white card-img-top" alt="..."></img>
-                    <div class="card-body">
-                        <h5 class="card-title fw-bold">LIEMPO INASAL</h5>
-                        <p class="card-text fs-5">₱ 89</p>
-                        <p class="card-text">
-                            <small class="text fs-6">solo</small></p>
-                        <a href="select" class="btn btn-light">Select</a>
+                <div className="card container">
+                    <img src={require('../../assets/liempo.jpg')} className="img-thumbnail card card-span text-white card-img-top" alt="..."></img>
+                    <div className="card-body">
+                        <h5 className="card-title fw-bold">LIEMPO INASAL</h5>
+                        <p className="card-text fs-5">₱ 89</p>
+                        <p className="card-text">
+                            <small className="text fs-6">solo</small></p>
+                        <button className="btn btn-light" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu">Select</button>
+                    </div>
+
+                    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasMenu" aria-labelledby="offcanvasMenuLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasMenuLabel">Menu</h5>
+                            <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <p className="text-dark">This is the content of my off-canvas menu.</p>
+                        </div>
                     </div>
                 </div>
 
-                <div class="card">
-                    <img src={require('../../assets/barbeque.jpg')} class="img-thumbnail h-100 card card-span h-100 text-white card-img-top" alt="..."></img>
-                    <div class="card-body">
-                        <h5 class="card-title fw-bold">PORK BBQ</h5>
-                        <p class="card-text fs-5">₱ 25</p>
-                        <p class="card-text">
-                            <small class="text fs-6">solo</small></p>
-                        <a href="select" class="btn btn-light">Select</a>
+                <div className="card">
+                    <img src={require('../../assets/barbeque.jpg')} className="img-thumbnail card card-span text-white card-img-top" alt="..."></img>
+                    <div className="card-body">
+                        <h5 className="card-title fw-bold">PORK BBQ</h5>
+                        <p className="card-text fs-5">₱ 25</p>
+                        <p className="card-text">
+                            <small className="text fs-6">solo</small></p>
+                        <button className="btn btn-light" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu">Select</button>
+                    </div>
+
+                    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasMenu" aria-labelledby="offcanvasMenuLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasMenuLabel">Menu</h5>
+                            <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <p className="text-dark">This is the content of my off-canvas menu.</p>
+                        </div>
                     </div>
                 </div>
             </div>
+            <Footer></Footer>
         </>
     );
 }
