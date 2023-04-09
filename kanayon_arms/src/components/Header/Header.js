d35cdf2e64ef871a70a17f8ee7d9b0f588b91111
kobellanes <118694734+kobellanes@users.noreply.github.com>
2023-04-09 18:47:26 +0800

    Connection of Header Page Establish

--- kanayon_arms/src/components/Header/Header.js
+++ kanayon_arms/src/components/Header/Header.js
@@ -23,8 +23,7 @@ function Header(props) {
                                     href="/">Home</a></li>
                                 <li className="nav-item px-2"><a className="nav-link fw-medium" href="/about">About Us</a></li>
                                 <li className="nav-item px-2"><a className="nav-link fw-medium" href="/ordernow">Order Now</a></li>
-                                <li className="nav-item px-2"><a className="nav-link fw-medium" href="/admin">Login/Register</a></li>
-                                <li className="nav-item px-2"><a className="nav-link fw-medium" href="/login">Trial</a></li>
+                                <li className="nav-item px-2"><a className="nav-link fw-medium" href="/login">Login/Register</a></li>
                             </ul>
                         </div>
                     </div>
