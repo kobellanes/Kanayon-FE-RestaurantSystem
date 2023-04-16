c9cc9fb9fb6c9cc1a3340d0bc66ac935666463aa
kobellanes <118694734+kobellanes@users.noreply.github.com>
2023-04-16 19:11:59 +0800

    Initial header

--- kanayon_arms/src/components/Header/Header.js
+++ kanayon_arms/src/components/Header/Header.js
@@ -1,35 +1,88 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
     return (
         <>
-            <main className="main" id="top">
+            <main className="llanesk-header">
+                <header className="llanesk-header-start">
 
-                <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3 d-block backdrop shadow-transition"
-                    data-navbar-on-scroll="data-navbar-on-scroll" style={{ backgroundColor: "rgba(255, 255,255, 0.75)" }}>
+                    <div className="container-fluid llanesk-con">
 
-                    <div className="container"><a className="navbar-brand d-inline-flex" href="/">
-                        <span className="text-1000 fs-0 fw-bold ms-2">Kanayon</span></a>
-
-                        <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                            aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-
-                        <div className="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
-
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
-                                <li className="nav-item px-2"><a className="nav-link fw-medium active" aria-current="page"
-                                    href="/">Home</a></li>
-                                <li className="nav-item px-2"><a className="nav-link fw-medium" href="/about">About Us</a></li>
-                                <li className="nav-item px-2"><a className="nav-link fw-medium" href="/ordernow">Order Now</a></li>
-                                <li className="nav-item px-2"><a className="nav-link fw-medium" href="/login">Login/Register</a></li>
-                            </ul>
+                        <div className="navb-logo">
+                            {/* <img src="/img/logo.png" alt="Logo"> */}
                         </div>
-                    </div>
-                </nav>
 
-            </main>
+                        <div className="llanesk-navb-items navb-items d-none d-xl-flex">
+
+                            <div className="llanesk-item">
+                                <a className="llanesk-links" href="/">Home</a>
+                            </div>
+
+                            <div className="llanesk-item">
+                                <a className="llanesk-links" href="/about">About</a>
+                            </div>
+
+                            <div className="llanesk-item">
+                                <a className="llanesk-links" href="/ordernow">Order Now</a>
+                            </div>
+
+                            <div className="llanesk-item-button">
+                                <a className="llanesk-links" href="/login" type="button">Login/Register</a>
+                            </div>
+                        </div>
+
+
+                        <div className="llanesk-hamburger mobile-toggler d-lg-none">
+                            <a className="llanesk-links" href="#" data-bs-toggle="modal" data-bs-target="#navbModal">
+                                <i className="fa-solid fa-bars"></i>
+                            </a>
+                        </div>
+
+                        <div className="modal fade" id="navbModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div className="llanesk-modal modal-dialog">
+                                <div className="llanesk-modal-content modal-content">
+
+                                    <div className="llanesk-m-header modal-header">
+                                        {/* <img src="/img/logo-variant.png" alt="Logo"> */}
+                                        <button type="button" className="llanesk-btn-close btn-close" data-bs-dismiss="modal" aria-label="Close"><i
+                                            className="fa-solid fa-xmark"></i></button>
+                                    </div>
+
+                                    <div className="llanesk-modal-body modal-body">
+
+                                        <div className="llanesk-modal-line">
+                                            <i className="fa-solid fa-house"></i><a className="llanesk-links" href="/">Home</a>
+                                        </div>
+
+                                        <div className="llanesk-modal-line">
+                                            <i className="fa-solid fa-bell-concierge"></i><a className="llanesk-links" href="/about">About Us</a>
+                                        </div>
+
+                                        <div className="llanesk-modal-line">
+                                            <i className="fa-solid fa-file-lines"></i> <a className="llanesk-links" href="/ordernow">Order Now</a>
+                                        </div>
+
+                                        <a href="/login" className="llanesk-links llanesk-navb-button navb-button" type="button">Login/Register</a>
+                                    </div>
+
+                                    <div className="llanesk-mobile-modal-footer mobile-modal-footer">
+
+                                        <a target="_blank" href="#"><i className="fa-brands fa-instagram"></i></a>
+                                        <a target="_blank" href="#"><i className="fa-brands fa-linkedin-in"></i></a>
+                                        <a target="_blank" href="#"><i className="fa-brands fa-youtube"></i></a>
+                                        <a target="_blank" href="#"><i className="fa-brands fa-facebook"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </header>
+
+
+            </main >
+
         </>
     );
 }
