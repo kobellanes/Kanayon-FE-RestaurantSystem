0135298f52cece4fce736fb87d5e08d518be633c
kobellanes <118694734+kobellanes@users.noreply.github.com>
2023-04-17 20:27:56 +0800

    Responsiveness Updated Again

--- kanayon_arms/src/components/Header/Header.js
+++ kanayon_arms/src/components/Header/Header.js
@@ -9,24 +9,27 @@ function Header() {
                 <div className="llanesk-navb-text ms-5 mt-3 d-flex justify-content-center align-items-center" data-navbar-on-scroll="data-navbar-on-scroll">
                     <div className="d-flex align-items-center mt-1">
                         <img className="rounded-pill llanesk-logo" src={require('../../assets/logo.jpg')}></img>
-                        <img src={require('../../assets/text.png')}></img>
+                        <img className="llanesk-logotext" src={require('../../assets/text.png')}></img>
                     </div>
 
 
-                    <div className="llanesk-con">
+                    <div className="llanesk-con me-5">
 
-                        <div className="container-fluid llanesk-navb-items align-items-center d-none d-xl-flex d-lg-flex justify-content-end">
+                        <div className="container-fluid llanesk-navb-items align-items-center d-none d-xl-flex d-lg-flex justify-content-end me-lg-5">
 
-                            <div className="llanesk-item fs-4">
-                                <a className="llanesk-links text-decoration-none fw-light" href="/">Home</a>
+                            <div className="llanesk-item fs-4 d-flex flex-row">
+                                <a href="/" className="llanesk-links fa-solid fa-house d-xxl-none d-xl-none d-lg-flex text-decoration-none fs-2"></a>
+                                <a className="llanesk-links text-decoration-none fw-light d-lg-none d-xl-flex" href="/">Home</a>
                             </div>
 
-                            <div className="llanesk-item fs-4">
-                                <a className="llanesk-links text-decoration-none fw-light" href="/about">About</a>
+                            <div className="llanesk-item fs-4 d-flex flex-row">
+                                <a href="about" className="llanesk-links fa-solid fa-address-card d-xxl-none d-xl-none d-lg-flex text-decoration-none fs-2"></a>
+                                <a className="llanesk-links text-decoration-none fw-light d-lg-none d-xl-flex" href="/about">About</a>
                             </div>
 
-                            <div className="llanesk-item fs-4">
-                                <a className="llanesk-links text-decoration-none fw-light" href="/ordernow">Order Now</a>
+                            <div className="llanesk-item fs-4 d-flex flex-row">
+                                <a href="ordernow" className="llanesk-links fa-solid fa-cart-shopping d-xxl-none d-xl-none d-lg-flex text-decoration-none fs-2"></a>
+                                <a className="llanesk-links text-decoration-none fw-light d-lg-none d-xl-flex" href="/ordernow">Order Now</a>
                             </div>
 
                             <div className="llanesk-item-button fs-4">
@@ -48,7 +51,7 @@ function Header() {
                             <div className="llanesk-modal-content modal-content overflow-y-scroll">
 
                                 <div className="llanesk-m-header modal-header d-flex text-white my-0 mx-auto py-4 justify-content-center">
-                                    <img className="rounded-pill llanesk-logo" src={require('../../assets/logo.jpg')}></img>
+                                    <img className="rounded-pill llanesk-logos" src={require('../../assets/logo.jpg')}></img>
                                     <img className="llanesk-modalpic align-items-center mt-2" src={require('../../assets/text.png')}></img>
                                     <button type="button" className="llanesk-btn-close btn-close pb-3 pe-2" data-bs-dismiss="modal" aria-label="Close"><i
                                         className="fa-solid fa-xmark fs-5 text-light"></i></button>
@@ -57,18 +60,18 @@ function Header() {
                                 <div className="llanesk-modal-body modal-body mx-auto mt-5 pt-5">
 
                                     <div className="llanesk-modal-line d-flex py-2">
-                                        <i href="/" className="fa-solid fa-house fs-2 ps-2 pb-1 me-3"></i><a className="text-decoration-none fs-6" href="/">Home</a>
+                                        <i className="fa-solid fa-house fs-2 ps-2 pb-1 me-3"></i><a className="text-decoration-none fs-6" href="/">Home</a>
                                     </div>
 
                                     <div className="llanesk-modal-line d-flex py-2">
-                                        <i className="fa-solid fa-bell-concierge fs-2 ps-2 pb-1 me-3"></i><a className="text-decoration-none fs-6" href="/about">About Us</a>
+                                        <i className="fa-solid fa-address-card fs-2 ps-2 pb-1 me-3"></i><a className="text-decoration-none fs-6" href="/about">About Us</a>
                                     </div>
 
                                     <div className="llanesk-modal-line d-flex py-2">
-                                        <i className="ms-1 fa-solid fa-file-lines fs-2 ps-2 pb-1 me-3"></i> <a className="text-decoration-none fs-6" href="/ordernow">Order Now</a>
+                                        <i className="fa-solid fa-cart-shopping fs-2 ps-2 pb-1 me-3"></i> <a className="text-decoration-none fs-6" href="/ordernow">Order Now</a>
                                     </div>
 
-                                    <a href="/login" className="llanesk-links text-decoration-none llanesk-navb-button text-light d-flex justify-content-center align-items-center fs-5" type="button">Login/Register</a>
+                                    <a href="/login" className="text-decoration-none llanesk-navb-button text-light d-flex justify-content-center align-items-center fs-5" type="button">Login/Register</a>
                                 </div>
 
                                 <div className="llanesk-mobile-modal-footer mobile-modal-footer d-flex justify-content-center mt-5 fs-2">
---
+++ kanayon_arms/src/components/UserList/UserList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AdminSideBar from '../AdminSideBar/AdminSideBar';
+
+function UserList() {
+    return (
+        <>
+            <AdminSideBar>
+                <h1 className="me-5 pe-5">Try</h1>
+            </AdminSideBar>
+
+
+
+        </>
+    );
+}
+
+export default UserList;
