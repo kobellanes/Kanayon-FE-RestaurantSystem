de274cd509dfd9fb6f778511fbc7aa2dd0003990
kobellanes <118694734+kobellanes@users.noreply.github.com>
2023-04-17 11:01:46 +0800

    HomePage Trial

--- kanayon_arms/src/components/HomePage/HomePage.js
+++ kanayon_arms/src/components/HomePage/HomePage.js
@@ -2,12 +2,7 @@ import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import './HomePage.css';
-
-const backgroundImage = {
-    backgroundImage: "url(" + require('../../bghome.jpg') + ")",
-    backgroundPosition: 'center',
-    backgroundSize: 'cover'
-}
+import slide1 from '../../assets/homeslide.jpg';
 
 function HomePage() {
     return (
@@ -18,7 +13,9 @@ function HomePage() {
 
             <section id="home" className="austriak-bg d-flex align-items-center">
                 <div className="py-11 bg-light-gradient border-bottom border-white border-5"></div>
-                <div className="bg-holder overlay overlay-light" style={backgroundImage}> </div>
+                <div className="bg-holder overlay overlay-light"> </div>
+
+                <img src={slide1}></img>
 
                 <div className="container position-relative text-center text-lg-start" data-aos="zoom-in" data-aos-delay="100">
                     <div className="row">
