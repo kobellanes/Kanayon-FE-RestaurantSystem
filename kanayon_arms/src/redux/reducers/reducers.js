9d34263236faab9fce57856c2bfcab9595549af7
Monique Aira Desengano <moniqueaira66@gmail.com>
2023-04-19 12:06:18 +0800

    List of Users UI updated

--- kanayon_arms/src/redux/reducers/reducers.js
+++ kanayon_arms/src/redux/reducers/reducers.js
@@ -4,14 +4,44 @@ const userInitialState = {
     users: [
         {
             id: 1,
-            user_name: "Juan Dela Cruz",
+            user_name: "Khenn Kobe",
+            email_add: "khennkobe@gmail.com",
+            gcash_num: "09934726287",
+            address: "Brgy. Ilog, Sto. Tomas, Batangas",
             isBan: 0,
         },
         {
             id: 1,
-            user_name: "Manny Pacquiao",
+            user_name: "Kyla Ciara",
+            email_add: "kylaciara@gmail.com",
+            gcash_num: "09998867891",
+            address: "Brgy. Looban, Tanauan City, Batangas",
             isBan: 0,
-        }
+        },
+        {
+            id: 1,
+            user_name: "Sky Lucido",
+            email_add: "skyers@gmail.com",
+            gcash_num: "09164261711",
+            address: "Brgy. Bangag, Tanauan City, Batangas",
+            isBan: 0,
+        },
+        {
+            id: 1,
+            user_name: "Monique Aira",
+            email_add: "moniqueaira@gmail.com",
+            gcash_num: "09557265172",
+            address: "Brgy. Puyat, Tanauan City, Batangas",
+            isBan: 0,
+        },
+        {
+            id: 1,
+            user_name: "Lomi House",
+            email_add: "lomian@gmail.com",
+            gcash_num: "772-5643",
+            address: "Brgy. Santolan, Tanauan City, Batangas",
+            isBan: 0,
+        },
     ]
 }
 
