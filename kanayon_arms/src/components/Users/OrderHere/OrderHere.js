47e37405fb411f43503a63ce59c6ec1c8aa67ce7
kobellanes <118694734+kobellanes@users.noreply.github.com>
2023-05-10 08:14:56 +0800

    Price Added

--- kanayon_arms/src/components/Users/OrderHere/OrderHere.js
+++ kanayon_arms/src/components/Users/OrderHere/OrderHere.js
@@ -10,7 +10,7 @@ function OrderHere() {
     const menus = useSelector((state) => state.allMenus.menus);
     const dispatch = useDispatch();
 
-    var a = "";
+    const a = "", b = "", c = "";
 
     const [menu_pic, setMenu_pic] = useState('');
     const [menu_name, setMenu_name] = useState('');
@@ -20,9 +20,13 @@ function OrderHere() {
     const [orderOrig, setOrderOrig] = useState('');
 
     const [newOrders, setNewOrders] = useState('');
-    const [oldOrders, setOldOrders] = useState('\n');
+    const [oldOrders, setOldOrders] = useState('');
+
+    const [newPrice, setNewPrice] = useState('');
+    const [oldPrice, setOldPrice] = useState('0');
 
     const [orderPrompt, setOrderPrompt] = useState("No Orders");
+    const [pricePrompt, setPricePrompt] = useState('');
 
     const fetchMenus = async () => {
         http.get('menus').then(result => {
@@ -54,19 +58,37 @@ function OrderHere() {
 
         const addOrder = menus.at(idn);
 
-        var a = addOrder.menu_name;
+        // const regEx = ['\n'];
+        // let textValue = stringToReplace.replace(/\\n/g,'\n');
 
-        setNewOrders(a + " " + oldOrders);
+        // const a = addOrder.menu_name;
+
+        // let textValue = a.replace(/\\n/g, '\n') + oldOrders;
+
+        // console.log(textValue);
+
+        // setNewOrders(textValue);
+
+        const a = addOrder.menu_name;
+        setNewOrders(a + oldOrders);
 
         setOldOrders(oldOrders + " " + a);
 
         setOrderPrompt("Orders:");
 
-        if (idn != -1) {
+        setPricePrompt("Total Price:")
 
+        const b = parseInt(addOrder.menu_price, 10);
 
+        const c = parseInt(oldPrice);
 
-        }
+        const d = b + c;
+
+        setNewPrice(d);
+
+        setOldPrice(d);
+
+        // c = c + b;
 
 
     }
@@ -234,7 +256,11 @@ function OrderHere() {
 
                     <div className="px-4 flex-flex-column">
                         <p>{orderPrompt}</p>
-                        <p>{newOrders}<br></br></p>
+                        <p>{newOrders}</p>
+
+                        <br></br>
+                        <p>{pricePrompt}{newPrice}</p>
+
                     </div>
 
                 </div>
@@ -268,194 +294,11 @@ function OrderHere() {
                 </div>
             </div>
 
-
-
-
-
-            {/* <div className="col">
-                        <div className="LucidoML-orderhere-card card d-flex align-items-center justify-content-center mb-3">
-                            <div className="row g-0">
-                                <div className="col-md-6 col-sm-6 py-3 ps-2 text-center">
-                                    <img src={require('../../../assets/paa.jpg')} className="LucidoML-orderhere-img-thumbnail card card-span text-white card-img-top" alt="..."></img>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="card-body">
-                                        <h5 className="LucidoML-orderhere-card-title">PAA INASAL</h5>
-                                        <p className="card-text text-wrap">
-                                            Paa Chicken Inasal with 1 rice
-                                        </p>
-                                        <p className="card-text">
-                                            <span className="LucidoML-orderhere-text-title">₱119</span>
-                                        </p>
-                                        <br></br>
-                                        <div className="d-flex justify-content-end align-items-end">
-                                            <button className="LucidoML_minus_btn btn text-decoration-none me-3"><a href="#"><i className="fa-solid fa-minus"></i></a></button>
-                                            <button className="LucidoML_plus_btn btn"><a href="#"><i className="fa-solid fa-plus"></i></a></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> */}
-
-            {/* <div className="col">
-                        <div className="LucidoML-orderhere-card card d-flex align-items-center justify-content-center mb-3">
-                            <div className="row g-0">
-                                <div className="col-md-6 col-sm-6 py-3 ps-2 text-center">
-                                    <img src={require('../../../assets/barbeque.jpg')} className="LucidoML-orderhere-img-thumbnail card card-span text-white card-img-top" alt="..."></img>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="card-body">
-                                        <h5 className="LucidoML-orderhere-card-title card-title">BARBEQUE</h5>
-                                        <p className="card-text text-wrap">
-                                            Barbeque with 1 rice
-                                        </p>
-                                        <p className="card-text">
-                                            <span className="LucidoML-orderhere-text-title">₱79</span>
-                                        </p>
-                                        <br></br><br></br>
-                                        <div className="d-flex justify-content-end align-items-end">
-                                            <button className="LucidoML_minus_btn btn text-decoration-none me-3"><a href="#"><i className="fa-solid fa-minus"></i></a></button>
-                                            <button className="LucidoML_plus_btn btn"><a href="#"><i className="fa-solid fa-plus"></i></a></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> */}
-
-            {/* <div className="col">
-                        <div className="LucidoML-orderhere-card card d-flex align-items-center justify-content-center mb-3">
-                            <div className="row g-0">
-                                <div className="col-md-6 col-sm-6 py-3 ps-2 text-center">
-                                    <img src={require('../../../assets/sisig.jpg')} className="LucidoML-orderhere-img-thumbnail card card-span text-white card-img-top" alt="..."></img>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="card-body">
-                                        <h5 className="LucidoML-orderhere-card-title card-title">SISIG</h5>
-                                        <p className="card-text text-wrap">
-                                            Kanayon Sisig with 1 rice
-                                        </p>
-                                        <p className="card-text">
-                                            <span className="LucidoML-orderhere-text-title">₱119</span>
-                                        </p>
-                                        <br></br>
-                                        <div className="d-flex justify-content-end align-items-end">
-                                            <button className="LucidoML_minus_btn btn text-decoration-none me-3"><a href="#"><i className="fa-solid fa-minus"></i></a></button>
-                                            <button className="LucidoML_plus_btn btn"><a href="#"><i className="fa-solid fa-plus"></i></a></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> */}
-
-            {/* <div className="col">
-                        <div className="LucidoML-orderhere-card card d-flex align-items-center justify-content-center mb-3">
-                            <div className="row g-0">
-                                <div className="LucidoML-talong col-sm-6 col-md-6 py-3 ps-2 text-center">
-                                    <img src={require('../../../assets/talong.jpg')} className="LucidoML-orderhere-img-thumbnail card card-span text-white card-img-top" alt="..."></img>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="card-body">
-                                        <h5 className="LucidoML-orderhere-card-title card-title">ENSALADANG TALONG</h5>
-                                        <p className="LucidoML-des card-text text-wrap">
-                                            Ensaladang talong pampagana meals</p>
-                                        <p className="card-text">
-                                            <span className="LucidoML-orderhere-text-title">₱55</span>
-                                        </p>
-                                        <div className="d-flex justify-content-end align-items-end">
-                                            <button className="LucidoML_minus_btn btn text-decoration-none me-3"><a href="#"><i className="fa-solid fa-minus"></i></a></button>
-                                            <button className="LucidoML_plus_btn btn"><a href="#"><i className="fa-solid fa-plus"></i></a></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> */}
-
-            {/* <div className="col">
-                        <div className="LucidoML-orderhere-card card d-flex align-items-center justify-content-center mb-3">
-                            <div className="row g-0">
-                                <div className="col-md-6 col-sm-6 py-3 ps-2 text-center">
-                                    <img src={require('../../../assets/halohalo.jpg')} className="LucidoML-orderhere-img-thumbnail card card-span text-white card-img-top" alt="..."></img>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="card-body">
-                                        <h5 className="LucidoML-orderhere-card-title card-title">HALO-HALO</h5>
-                                        <p className="card-text text-wrap">
-                                            Extra creamy halo-halo with ice cream, leche flan and ube</p>
-                                        <p className="card-text">
-                                            <span className="LucidoML-orderhere-text-title">₱45</span>
-                                        </p>
-                                        <div className="d-flex justify-content-end align-items-end">
-                                            <button className="LucidoML_minus_btn btn text-decoration-none me-3"><a href="#"><i className="fa-solid fa-minus"></i></a></button>
-                                            <button className="LucidoML_plus_btn btn"><a href="#"><i className="fa-solid fa-plus"></i></a></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> */}
-
-            {/* <div className="col">
-                        <div className="LucidoML-orderhere-card card d-flex align-items-center justify-content-center mb-3">
-                            <div className="row g-0">
-                                <div className="col-md-6 col-sm-6 py-3 ps-2 text-center">
-                                    <img src={require('../../../assets/icetea.jpg')} className="LucidoML-orderhere-img-thumbnail card card-span text-white card-img-top" alt="..."></img>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="card-body">
-                                        <h5 className="LucidoML-orderhere-card-title card-title">ICE TEA</h5>
-                                        <p className="card-text text-wrap">
-                                            1 Glass of ice tea</p>
-                                        <p className="card-text">
-                                            <span className="LucidoML-orderhere-text-title">₱15</span>
-                                        </p>
-                                        <br></br><br></br>
-                                        <div className="d-flex justify-content-end align-items-end">
-                                            <button className="LucidoML_minus_btn btn text-decoration-none me-3"><a href="#"><i className="fa-solid fa-minus"></i></a></button>
-                                            <button className="LucidoML_plus_btn btn"><a href="#"><i className="fa-solid fa-plus"></i></a></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> */}
-
-            {/* <div className="col">
-                        <div className="LucidoML-orderhere-card card d-flex align-items-center justify-content-center mb-3">
-                            <div className="row g-0">
-                                <div className="col-md-6 col-sm-6 py-3 ps-2 text-center">
-                                    <img src={require('../../../assets/softdrinks.jpg')} className="LucidoML-orderhere-img-thumbnail card card-span text-white card-img-top" alt="..."></img>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="card-body">
-                                        <h5 className="LucidoML-orderhere-card-title card-title">SOFTDRINKS</h5>
-                                        <p className="card-text text-wrap">
-                                            Any of your choice!</p>
-                                        <p className="card-text">
-                                            <span className="LucidoML-orderhere-text-title">₱20</span>
-                                        </p>
-                                        <br></br><br></br>
-                                        <div className="d-flex justify-content-end align-items-end">
-                                            <button className="LucidoML_minus_btn btn text-decoration-none me-3"><a href="#"><i className="fa-solid fa-minus"></i></a></button>
-                                            <button className="LucidoML_plus_btn btn"><a href="#"><i className="fa-solid fa-plus"></i></a></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div> */}
-
-
-
-
-
             <Footer>
             </Footer>
+
         </>
+
     );
 }
 
