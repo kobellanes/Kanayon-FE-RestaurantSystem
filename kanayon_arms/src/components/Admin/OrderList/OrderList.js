255375a8f39a3c040789ecc9250f12cc475ea94b
kobellanes <118694734+kobellanes@users.noreply.github.com>
2023-05-16 09:03:26 +0800

    Initial Dashboard

--- kanayon_arms/src/components/Admin/OrderList/OrderList.js
+++ kanayon_arms/src/components/Admin/OrderList/OrderList.js
@@ -76,7 +76,7 @@ function OrderList() {
                                         <ul className="dropdown-menu w-100">
                                             <li><a className="llanesk-userlist-dropdown-item dropdown-item text-dark" href="orderlist"><i className="fa-solid fa-pen me-2"></i>Pending Orders</a></li>
                                             <li><a className="llanesk-userlist-dropdown-item dropdown-item text-dark" href="receive"><i className="fa-brands fa-google-pay me-2"></i>Paid Orders</a></li>
-                                            <li><a className="llanesk-userlist-dropdown-item dropdown-item text-dark" href="#"><i className="fa-solid fa-check me-2"></i>Completed Orders</a></li>
+                                            <li><a className="llanesk-userlist-dropdown-item dropdown-item text-dark" href="completed"><i className="fa-solid fa-check me-2"></i>Completed Orders</a></li>
                                         </ul>
                                     </li>
 
